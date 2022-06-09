6b48bc359d4f13d94fce667a5d6b72b8a85c4dd1
Aprax-wq <73428662+Aprax-wq@users.noreply.github.com>
2022-06-09 20:39:25 +0300

    Add files via upload

---
+++ src/Commands/Setup.js
@@ -0,0 +1,11 @@
+class Setup {
+  constructor () {
+    (this.name = "kur", this.alias = ["setup"]);
+  };
+  
+  async operate ({client, message, args, author, member, config}) { 
+    
+  };
+};
+
+module.exports = Setup;
