06442cc18e17d857a6e7b86c5b52699031196520
Aprax-wq <73428662+Aprax-wq@users.noreply.github.com>
2022-06-09 21:46:49 +0300

    Create Gacıfind.js

---
+++ src/Commands/Gacıfind.js
@@ -0,0 +1,19 @@
+class Gacıbul {
+  constructor () {
+    (this.name = "gacıbul", this.alias = ["kızbul"]);
+  };
+  
+  async operate ({client, message, args, author, member, config}) { 
+    if (author.id !== config.Import.Owners[0]) return;
+
+    let isim = args[0];
+    let yaş = args[1];
+    let i =0;
+
+    if (!isim || !yaş) return message.channel.send("ya orospu çocuğu detaylı Arama için isim yaş girmen laızm hem girmiyon hem de uğraşa sokuyon")
+    let kızlar = message.guild.members.cache.filter(x => x.displayName.includes(isim) || x.displayName.includes(yaş)).split(0,9).map(x=> i+1 + x.user.discriminator);
+    message.channel.send(`al kardsm cabuk bul yengeyi \n\n ${kızlar} `)
+  };
+};
+
+module.exports = Gacıbul;
