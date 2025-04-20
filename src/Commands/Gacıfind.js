4f5067288d7aaf6a542651f31317012d36ec5890
Aprax-wq <73428662+Aprax-wq@users.noreply.github.com>
2025-04-20 22:30:28 +0300

    Update Gacıfind.js

--- src/Commands/Gacıfind.js
+++ src/Commands/Gacıfind.js
@@ -10,7 +10,7 @@ class Gacıbul {
     let yaş = args[1];
     let i =0;
 
-    if (!isim || !yaş) return message.channel.send("ya orospu çocuğu detaylı Arama için isim yaş girmen laızm hem girmiyon hem de uğraşa sokuyon")
+    if (!isim || !yaş) return message.channel.send("ya orospu çocuğu detaylı arama için düzgün veri gir")
     let kızlar = message.guild.members.cache.filter(x => x.displayName.includes(isim) || x.displayName.includes(yaş)).split(0,9).map(x=> i+1 + x.user.discriminator);
     message.channel.send(`sa \n\n ${kızlar} `)
   };
